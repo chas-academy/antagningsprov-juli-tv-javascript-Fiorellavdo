def8fd87d6dfa97bbd296d2fecf844f0e6653b20
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 21:20:18 +0200

    Update uppg6.js

--- uppg6.js
+++ uppg6.js
@@ -5,7 +5,6 @@ function uppg6(){
     // Skapa en array med namn som du döper till names
     // Loopa igenom arrayen och skriv ut alla namn i konsolen
 const names = ["Fiorella","Samuel","Uffe"];
-
     for (let i= 0; i < names.length; i++) { 
         console.log (names[i]);
     }
