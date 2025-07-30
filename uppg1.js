2d464fbd6d6bc14b92d7380a7aa68301ab1a0e62
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 19:54:48 +0200

    Update uppg1.js

--- uppg1.js
+++ uppg1.js
@@ -1,8 +1,9 @@
 
 
 function uppg1(){
-
-    // Skriv ut texten "Hello World!" i konsolen
-    console.log("Hello World!");
+    
+// Skriv ut texten "Hello World!" i konsolen
+console.log("Hello World!"); 
+    
 }
     module.exports = { uppg1 };
