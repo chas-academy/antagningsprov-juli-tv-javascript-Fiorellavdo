578151be40e34b010360789c5a2a1de6781d3447
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 20:07:37 +0200

    Update uppg4.js

--- uppg4.js
+++ uppg4.js
@@ -1,11 +1,14 @@
 
 function uppg4() {
-  
+  Const person = { 
+    Name: "Fiorella"
+    age: 25
+};
     // deklarera ett objekt med namnet person
     // objektet ska ha två properties, name och age
     // tilldela age 25
     // skriv ut med dotnotation åldern i konsolen
-
+Console.log(person.age);
 
 }
 
