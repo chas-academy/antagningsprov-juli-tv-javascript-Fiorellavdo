c268b32629d21bc582896ef0dcff9e7180dd5981
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 22:47:07 +0200

    Update uppg9.js

--- uppg9.js
+++ uppg9.js
@@ -1,21 +1,22 @@
 
 
-function uppg9 (arr) {
+function uppg9 () {
     // skapa en funktion som heter sort och tar in en array av siffror som argument
-     for (let i = 0; i < arr.length; i++) {
-    if (arr[i] % 2 === 0) {
-      console.log(arr[i] + " jämt");
-    } else {
-      console.log(arr[i] + " udda");
+   function kollaOmJämtEllerUdda(arr) {
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i] % 2 === 0) {
+        console.log(arr[i] + " jämt");
+      } else {
+        console.log(arr[i] + " udda");
+      }
     }
   }
-}
     // i funktionen du loopa igenom arrayen och kolla med en if-sats om  talet är jämt eller udda
     // i if-sattsen skriv ut i konsolen resultatet
     // resultatet ska loggas i formatet: talet och sen ordet "jämt" eller "udda"
     // anropa funktionen och skicka med en array som argument
 const siffror = [ 1,2,3,4,5,6]
-sort (siffror); 
+kollaOmJämtEllerUdda (siffror); 
 }
 
 module.exports = { uppg9 };
