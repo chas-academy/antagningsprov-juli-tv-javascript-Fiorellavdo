639b9e3a4b3526842087f528725301dfacc0d427
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 21:22:49 +0200

    Update uppg8.js

--- uppg8.js
+++ uppg8.js
@@ -5,26 +5,26 @@ function uppg8(){
 // skapa en array med fem person obkjet
 // varje person objekt ska ha två properties, name och age
 const personer = [
-  { name: "Samuel", age: 26 },
-  { name: "Uffe", age: 35 },
-  { name: "Fiorella", age: 45 },
-  { name: "Viktor", age: 28 },
-  { name: "Andreas", age: 31 },
-  
-];
+    { name: "Samuel", age: 26 },
+    { name: "Uffe", age: 35 },
+    { name: "Fiorella", age: 45 },
+    { name: "Viktor", age: 28 },
+    { name: "Andreas", age: 31 },
+  ];
+
  
 // skapa en fuktion som tar in en array som argument
 // skapa ett konditionsvillkor som gör att det bara är namnet på personerna som är
 //  över 30 år som skrivs ut i konsolen
 function skrivUtNamnOver30(arr) {
-  for (let i = 0; i < arr.length; i++) {
-     if (arr[i].age > 30){
-       console.log (arr[i].name);
-     }
+    for (let i = 0; i < arr.length; i++) {
+      if (arr[i].age > 30) {
+        console.log(arr[i].name);
+      }
+    }
   }
-}
 // anropa funktionen och skicka med arrayen som argument
-skrivUtNamnOver30 (personer);
+ skrivUtNamnOver30(personer);
 }
 
 module.exports = { uppg8 };
