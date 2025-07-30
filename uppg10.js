16b5a3fa26b3735fe2bfd4c105f7df18b7aa09b0
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 20:48:52 +0200

    Update uppg10.js

--- uppg10.js
+++ uppg10.js
@@ -1,17 +1,33 @@
-function uppg10(){
+function uppg10() {
     const numbers = [5, 12, 7, 10, 20, 9, 11];
   
     // skapa tre arrayer - bigEven, smallOdd, other
+    const bigEven = [];
+    const smallOdd = [];
+    const other = [];
     // loopa igenom numbers och placera talen i respektive array
+for (let i = 0; i < numbers.length; i++) {
+    const num = numbers [i]
+
+    if(num > 10 && % 2 === 0) {
+        bigEven.push (num); 
+    } else if (num < 10 && num % 2 !==0) {
+        smallOdd.push (num); 
+    } else { 
+        other.push (num);
+    }
+}
     // bigEven ska innehålla tal större än 10 och jämnt delbara med 2
     // smallOdd ska innehålla tal mindre än 10 och udda
     // other ska innehålla alla andra tal
     // returnera ett objekt med de tre arrayerna
 
-  
-    
-  
-  }
+  Return { 
+      bigEven,
+      smallOdd,
+      other
+   };  
+ }
   
   module.exports = { uppg10 };
   
