8dbca59ff1837f464efeb72f6232d6d63f86a5be
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 20:59:49 +0200

    Update uppg7.js

--- uppg7.js
+++ uppg7.js
@@ -1,13 +1,14 @@
 
 
 function uppg7() {
-Function sum(a,b) {
+   
+function sum(a,b) {
    return a + b;
 }
     // skapa en funktion som tar in två tal som argument
     // funktionen ska returnera summan av talen
     // kalla på funktionen i en return statement och ge den parametrarna 5 och 10
-Return sum (5,10);
+return sum (5,10);
 }
 
 module.exports = { uppg7 };
