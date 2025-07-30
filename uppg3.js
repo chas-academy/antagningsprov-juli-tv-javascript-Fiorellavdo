fb889bd56b6d85ce72bbd28f2737d2edc0daf355
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 20:58:21 +0200

    Update uppg3.js

--- uppg3.js
+++ uppg3.js
@@ -6,10 +6,10 @@ function uppg3() {
     // deklarera två variabler, a och b
     // tilldela värdena 5 och 10 till variablerna
     // skriv ut summan av a och b i konsolen
-Const a = 5; 
-Const b = 10; 
+const a = 5; 
+const b = 10; 
 
-    Console.log (a + b);
+    console.log (a + b);
 
 
 }
