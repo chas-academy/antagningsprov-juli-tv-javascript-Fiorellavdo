b83168beb7c9dd8410906ddd530e87d29f1a824b
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-30 21:06:50 +0200

    Update uppg2.js

--- uppg2.js
+++ uppg2.js
@@ -3,7 +3,7 @@ function uppg2() {
    // Skriv din kod här
    // deklarera en variable med ditt namn och skriv ut det i konsolen
 const namn = "Fiorella";
-console.long(Fiorella);
+console.log(namn);
 }
 
 module.exports = { uppg2 };
