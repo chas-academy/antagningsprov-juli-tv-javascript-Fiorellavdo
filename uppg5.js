34d7368bd4c94a4e466203d8887d64f63b847d83
Fiorellavdo <digiuliofiorella@gmail.com>
2025-07-31 12:15:01 +0200

    Update uppg5.js

--- uppg5.js
+++ uppg5.js
@@ -3,13 +3,12 @@ function uppg5() {
     // skriv en if-sats som jämför två tal
     // om det första talet är större än det andra, skriv ut "Det första talet är större än det andra"
     // annars skriv ut "Det andra talet är större än det första"
+
 const tal1 = 10;
 const tal2 = 5;
-    if (tal1 > tal2) {
-console.log("det första talet är större än det andra") 
-    } else { 
-        console.log("det andra talet är större än det första");
-    }
-}
 
-module.exports = { uppg5 };
+if (tal1 > tal2) {
+  console.log("det första talet är större än det andra");
+} else { 
+  console.log("det andra talet är större än det första");
+}
